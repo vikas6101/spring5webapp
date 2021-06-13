2885a7ee928d487c54a93383c478f90990e9f21e
Vikas Singh <askforvikas@gmail.com>
2021-06-14 00:07:12 +0400

    Committing changes

--- src/main/java/guru/springframework/spring5webapp/Spring5webappApplication.java
+++ src/main/java/guru/springframework/spring5webapp/Spring5webappApplication.java
@@ -2,6 +2,13 @@ package guru.springframework.spring5webapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.thymeleaf.expression.Sets;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
 
 @SpringBootApplication
 public class Spring5webappApplication {
---
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -0,0 +1,47 @@
+package guru.springframework.spring5webapp.bootstrap;
+
+import guru.springframework.spring5webapp.domain.Author;
+import guru.springframework.spring5webapp.domain.Book;
+import guru.springframework.spring5webapp.domain.Publisher;
+import guru.springframework.spring5webapp.repositories.AuthorRepository;
+import guru.springframework.spring5webapp.repositories.BookRepository;
+import guru.springframework.spring5webapp.repositories.PublisherRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BootStrapData implements CommandLineRunner {
+
+    private final AuthorRepository authorRepository;
+    private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
+
+    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
+        this.authorRepository = authorRepository;
+        this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        Publisher publisher = new Publisher("Doremon publishing", "Street 1", "Dubai", "Dubai", "1150");
+        publisherRepository.save(publisher);
+        System.out.println("Number of publisher" + publisherRepository.count());
+
+        Author author = new Author("Eric", "Evans");
+        Book book = new Book("Domain driven deign", "123123");
+        author.getBooks().add(book);
+        book.getAuthors().add(author);
+
+        book.setPublisher(publisher);
+        publisher.getBooks().add(book);
+
+        authorRepository.save(author);
+        bookRepository.save(book);
+        publisherRepository.save(publisher);
+
+        System.out.println("Number of books" + bookRepository.count());
+
+    }
+}
